f1bf0ffc37ca09dbd2e44db7e1da5332fb039f22
Taiyuan No.5 Middle School OIers <ljt714285@gmail.com>
2015-08-25 15:35:01 +0800

    Create tocl.h

---
+++ tocl.h
@@ -0,0 +1,32 @@
+/*TOCL, a tiny/useful code library for OIers
+* Copyright(C) 2015, TYWZOIers
+* You should Read GPLv3 carefully before use the library
+*/
+
+/*
+* 代码库中的所有算法和数据结构都在NOIp范围内
+* For OIer 可以通过阅读源代码学习算法
+* For Programmer 可以通过调用函数和类快速的完成工作而不必编写算法
+*/
+
+/*文件说明
+* basic.h 包含一些基本操作
+* sort.h 包含NOIp常用排序算法实现
+* bignum.h 包含NOIp常用高精度运算
+* number_theory.h 包含NOIp范围内常用的数论
+* heap.h 包含二叉堆的相关算法
+* graph.h 包含NOIp范围内的图论算法
+*/
+
+/*
+Github地址： www.github.com/TYWZOIers/TOCL
+*/
+
+#pragma once
+
+#include "basic.h"
+#include "sort.h"
+#include "bignum.h"
+#include "number_theory.h"
+#include "heap.h"
+
